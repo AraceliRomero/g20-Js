9f7c68f4ad89bd1173e93e2db171e354905dd385
Araceli <araceli.romero85@gmail.com>
2022-06-29 22:27:01 -0500

    clase 1

---
+++ JS/bse.js
@@ -0,0 +1,43 @@
+//! Comentarios en Js
+    // Esto es un comentario 
+
+    /*
+    Esto es
+    un comentario
+    multilinea
+    */
+
+// ! Datos en Js 
+
+    // Identificador 
+    // Reglas o buenas practicas
+    //No inician con digitos 
+        // let 9 vairable = 9; -> Error
+    // No acepta palabra reservadas
+        // let else = "else"     => Error
+    // Consideracion, Sensible a las mayus y minus
+    // Buena practica; camelCase 99% -PascalCase -> Clases -CONSTANTE
+    // Buena practica; Nawing descriptivo -> let estudiantesKodemia = "koders"
+
+    // ! Variables y Constantes
+        // Variables = Mutabilidad
+        // Constantes = No cambia 
+
+        // Variables = palabras reservadas para declarar ; var y let 
+
+        let edadKoder = 25; 
+        var alturaKoder = 165
+
+        var alturaKoder = 185
+        
+        // palabraReservada Identificador operadorAsignacion -> Valor 
+        // let test = 1
+        // test = 2
+        // test = 9
+        //console.log(test)
+
+        console.log (edadKoder);
+        console.log(alturaKoder); 
+
+        // REcomendacion/ Buena practica siempre usar Let
+        console.log("a");
